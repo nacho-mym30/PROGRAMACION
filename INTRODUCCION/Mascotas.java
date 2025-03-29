d7253afb36dbef67271c59a42ab8d046ac3a4dd2
nacho_mym <luisigmolina30@gmail.com>
2025-03-28 21:39:34 -0400

    Agregado a introduccion

---
+++ INTRODUCCION/Mascotas.java
@@ -0,0 +1,12 @@
+public class Mascotas{
+    public int calcularMascotas(){
+        int gatitos = 24;
+        int cachorros = 18;
+        int mascAdop = 13;
+        int conejitos = 5;
+        int mascotas1 = gatitos + cachorros;
+        int mascotas2 = mascotas1 - mascAdop;
+        int mascTotal = mascotas2 + conejitos;
+        return mascTotal; 
+    }
+}
